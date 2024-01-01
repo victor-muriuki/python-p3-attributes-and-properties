75f038232863e6e5b0cf0a02c6e052b6a7f7bdfe
+ <+>
2024-01-01 14:29:12 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,21 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
+    def __init__ (self, name, breed = None) :
+        self.name = name
+        if not isinstance(name, str) or not (1<= len(name)<=25):
+            print ("Name must be a string between 1 and 25 characters.")
+            return 
+        
+        if breed and breed not in APPROVED_BREEDS:
+            print ("Breed must be in the list of approved breeds.")
+        
+        else:
+            self.breed = breed
+
+       
+
+
+        pass
     pass
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,12 @@ APPROVED_JOBS = [
 ]
 
 class Person:
+    def __init__(self, name) -> None:
+        self.name = name
+        
+        if not isinstance(name, str) or not (1<= len(name)<= 25):
+            print("Name must be string under 25 characters.")
+        return  
+          
+        pass
     pass
